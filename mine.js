983d1cf580dd063ecd8ba3db115a4c5ccdf29c65
Anastasia <novakanastasya@yandex.ru>
2018-01-13 22:22:33 +0300

    Change mine.js

--- mine.js
+++ mine.js
@@ -1,48 +1,44 @@
-const el = document.getElementById('timer');
-const startBtn = document.getElementById('start-btn');
-const stoptBtn = document.getElementById('stop-btn');
-const resetBtn = document.getElementById('reset-btn');
+let h1 = document.getElementsByTagName('h1')[0];
+let start = document.getElementById('start-btn');
+let stop = document.getElementById('stop-btn');
+let clear = document.getElementById('reset-btn');
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let pause;
 
-let intervalId = null;
-let startDate = null;
-
-
-startBtn.addEventListener('click', function() {
-    runTimer()
-});
-
-stoptBtn.addEventListener('click', () => {
-    stopTimer()
-});
-
-resetBtn.addEventListener('click', () => {
-    startDate = new Date();
-    drawTime(startDate)
-});
-
-const drawTime = (startDate) => {
-    const timeElapsed = new Date() - startDate;
-    const sec = Math.floor(timeElapsed / 1000);
-    const msec = timeElapsed - sec * 1000;
-    el.innerText = `${sec}.${msec}`
-};
-
-const runTimer = () => {
-    if (intervalId === null) {
-        if (startDate === null) {
-            startDate = new Date()
+const add = () => {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
         }
-        intervalId = setInterval(() => {
-            drawTime(startDate)
-        }, 1)
     }
+
+    h1.innerText = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" +
+        (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
+        (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
 };
 
-const stopTimer = () => {
-    if (intervalId !== null) {
-        clearInterval(intervalId);
-        intervalId = null
-    }
+const timer = () => {
+    pause = setTimeout(add, 1000);
 };
 
-drawTime(new Date());
+/* Start button */
+start.onclick = timer;
+
+/* Stop button */
+stop.onclick = () => {
+    clearTimeout(pause);
+};
+
+/* Clear button */
+clear.onclick = () => {
+    h1.innerText = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+};
